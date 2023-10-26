d643223e287efc17c21dedb7e964db55930dac7b
Krishna <115589158+Krishna7886@users.noreply.github.com>
2023-10-26 16:24:49 +0530

    Update Chess.js

--- Chess/Chess.js
+++ Chess/Chess.js
@@ -2,5 +2,7 @@
     var a = document.getElementById("val");
     var b = document.getElementById("top");
     b.innerHTML = a.value;
-    alert(`Eeeevvvvv ${a.value} Yu created a game yar`)
+  var alertv = `Eeeevvvvv ${a.value} You created a game yar`
+    alert(alertv)
+    alert("Eeeevvvvv "+a.value+" You created a game yar")
    }
