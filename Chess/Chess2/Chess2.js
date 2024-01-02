9daba877ef079dda27686881e102eb7a9a6d5111
Krishna <115589158+Krishna7886@users.noreply.github.com>
2024-01-02 20:45:30 +0530

    Update Chess2.js

--- Chess/Chess2/Chess2.js
+++ Chess/Chess2/Chess2.js
@@ -43,7 +43,7 @@ for(var i=0;i<8;i++){
     for(var j=0;j<8;j++){
         if(i%2 == 0 && j%2 == 0){
             box[i][j].style.backgroundColor="#ffffc0"
-            box[i][j].style.onhover
+            
         }
         if(i%2 == 1 && j%2 == 1){
             box[i][j].style.backgroundColor="#ffffc0"
