b5ab7669e618bbf81df8b60b28c07626bec49c1d
Krishna <115589158+Krishna7886@users.noreply.github.com>
2024-01-20 02:16:17 +0530

    Create chat.js

---
+++ Instagram/chatWindow/chat.js
@@ -0,0 +1,12 @@
+function back(){
+        alert("It's under construction");
+    }
+    function id(){
+        alert("You are trying to check the Id It's under construction");
+    }
+    function audioCall(){
+        alert("It's under Construction")
+    }
+    function videoCall(){
+        alert("It's under Construction")
+    }
